93bb1e3e865474d327f2ac0402d77a87d20aeda0
inggit-prakasa <inggit.prakasa123@gmail.com>
2021-01-14 23:59:38 +0700

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(cors());
+app.options('*', cors());
 
 // Middleware
 app.use(bodyParser.json());
