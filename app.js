2e43a4480468a0d4d28650be8de5e631ac0b8551
inggit-prakasa <inggit.prakasa123@gmail.com>
2021-01-14 23:01:44 +0700

    mongoose and mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // Middleware
 app.use(bodyParser.json());
@@ -26,6 +27,17 @@ app.post(`${api}/products`, (req, res) => {
     const newProduct = req.body
     res.send(newProduct)
 })
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Database Connection is Ready')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.listen(port, () => {
     console.log(`server is running in http://localhost:${port}`)
