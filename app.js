15cfb9961737474bb2ffe8f357f5b1bb75e0501f
inggit-prakasa <inggit.prakasa123@gmail.com>
2021-01-14 19:49:38 +0700

    parsing data

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Middleware
+app.use(bodyParser.json());
 
 require('dotenv/config');
 
@@ -7,8 +11,18 @@ const api = process.env.API_URL
 
 const port = 3000
 
-app.get(api + '/', (req, res) => {
-    res.send('Hello API !!')
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'Hair',
+        image: 'some_url',
+    }
+    res.send(product)
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body
+    res.send(newProduct)
 })
 
 app.listen(port, () => {
