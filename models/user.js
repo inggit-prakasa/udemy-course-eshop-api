60b7e30a399f2b231bce0e897fc90c55816dc485
inggit-prakasa <inggit.prakasa123@gmail.com>
2021-01-16 22:44:31 +0700

    user Schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,54 @@
 const mongoose = require('mongoose');
 
 const userScheme = mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    }
 })
+
+userScheme.virtual('id').get(function() {
+    return this._id.toHexString();
+});
+
+userScheme.set('toJSON', {
+    virtuals: true,
+});
 
 exports.User = mongoose.model('User', userScheme)
